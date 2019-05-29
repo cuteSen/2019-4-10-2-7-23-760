93de74789b5e4ccc79b3ed813bc391fb67b186ad
wh1621521613 <wh1621521613@163.com>
2019-05-29 16:08:39 +0800

    problem3

--- main/main.js
+++ main/main.js
@@ -1,4 +1,19 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var price = 6;
+    if(inputs.distance<=2)
+    {
+        price+=Math.round(inputs.parkTime*0.25)
+        return price;
+    }
+    if(inputs.distance>=8)
+    {
+        price+=Math.round(inputs.parkTime*0.25+(inputs.distance-8)*1.2+4.8)
+        return price;
+    }
+    else
+    {
+        price+=Math.round(inputs.parkTime*0.25+(inputs.distance-2)*0.8)
+        return price;
+    }
 };
